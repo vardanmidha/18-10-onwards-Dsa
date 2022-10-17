3f4073af1ca923cabb94a14891ae6e8c250582a0
vardanmidha <anshmidha2@gmail.com>
2022-10-17 14:17:59 +0530

    hyu

--- ewjbf.cpp
+++ ewjbf.cpp
@@ -7,10 +7,22 @@
 #define IOS ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0) 
 using namespace std ;
 
+
 int main()
 {
-    IOS;
     
-    cout<<"hello Worls";
+    IOS;
+
+
+    
+    
+
+cout<<"Hello world";
+
+// abd uirevbn 
+
+
+
+
 
 }
